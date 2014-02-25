8b915f2318d5d65517c4d509c8d40a66baa624af
TomAugspurger <thomas-augspurger@uiowa.edu>
2014-02-25 12:58:27 -0600

    setup.py didnt like topic

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Stats',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
