3af925c29082258f1db63e26d5175a99a001458e
Tom Augspurger <thomas-augspurger@uiowa.edu>
2014-03-19 14:14:54 -0500

    REF: move get_pro_matches to scripts

--- parsers.py
+++ parsers.py
@@ -1,12 +1,6 @@
 import argparse
 import re
-import os
 import json
-import pathlib
-
-from lxml import html
-
-from dota import api
 
 _start_hero_pattern = '\t"npc_dota_hero_'
 _start_item_pattern = '\t"item_'
@@ -126,65 +120,8 @@ def get_block(f, line, kind):
 
     return results
 
-
-def get_pro_matches():
-    # Find new match ids
-    url = "http://www.datdota.com/matches.php"
-    r = html.parse(url).getroot()
-
-    reg = re.compile(r'match.*(\d{9})$')
-    links = filter(lambda x: reg.match(x[2]), r.iterlinks())
-
-    here = os.path.dirname(__file__)
-    match_ids_path = os.path.join(here, 'data', 'pro', 'pro_match_ids.txt')
-
-    with open(match_ids_path, 'r') as f:
-        old_ids = f.readlines()
-
-    ids = (x[2].split('?q=')[-1] + '\n' for x in links)
-    new_ids = [x for x in ids if x not in old_ids]
-
-    with open(match_ids_path,  'a+') as f:
-        f.writelines(new_ids)
-
-    #--------------------------------------------------------------------------
-    # Get Match Details for new matches
-    with open(os.path.expanduser('~/') + 'Dropbox/bin/api-keys.txt') as f:
-        key = json.load(f)['steam']
-
-    h = api.API(key=key)
-
-    with open(match_ids_path, 'a+') as f:
-        match_ids = [x.strip() for x in f.readlines()]
-
-    f = pathlib.Path(__file__).absolute()
-    data_path = f.parent.joinpath(pathlib.Path('data/pro'))
-
-    matches = filter(None, map(lambda x: re.match(r'.*(\d{9}).json$', x),
-                               os.listdir(str(data_path))))
-    matches = [x.groups()[0] for x in matches]
-    new_matches = [x for x in match_ids if x not in matches]
-    details = {mid: h.get_match_details(mid) for mid in new_matches}
-
-    if not data_path.exists():
-        data_path.mkdir()
-
-    for k in details:
-        with (data_path / (str(k) + '.json')).open('w') as f:
-            json.dump(details[k].resp, f)
-
-    #--------------------------------------------------------------------------
-    # Insert into pro.db
-    from examples.sqlalchemyORM import update_db
-
-    update_db(data_path)
-
-    print("Added {}".format(new_ids))
-
-
 parser = argparse.ArgumentParser()
-parser.add_argument('kind', choices=['items', 'heroes', 'abilities',
-                                     'pro_matches'])
+parser.add_argument('kind', choices=['items', 'heroes', 'abilities'])
 
 
 if __name__ == '__main__':
@@ -193,7 +130,6 @@ if __name__ == '__main__':
 
     dispatch = {'items': parse_items,
                 'heroes': parse_heroes,
-                'abilities': parse_abilities,
-                'pro_matches': get_pro_matches
+                'abilities': parse_abilities
                 }
     dispatch[kind]()
--- scripts/get_pro_matches.py
+++ scripts/get_pro_matches.py
@@ -1,35 +1,69 @@
+# -*- coding: utf-8 -*-
+
 import os
 import re
 import json
+import pathlib
 
-import dota
+from lxml import html
+
 from dota import api
+from dota.helpers import cached_games
 
-with open(os.path.expanduser('~/Dropbox/bin/api-keys.txt')) as f:
-    key = json.load(f)['steam']
+def get_pro_matches():
+    # Find new match ids
+    url = "http://www.datdota.com/matches.php"
+    r = html.parse(url).getroot()
 
-h = api.API(key=key)
+    reg = re.compile(r'match.*(\d{9})$')
+    links = filter(lambda x: reg.match(x[2]), r.iterlinks())
 
-p = os.path.join(os.path.dirname(dota.__file__),
-                 'pro_match_ids.txt')
-with open(p) as f:
-    match_ids = [x.strip() for x in f.readlines()]
+    here = pathlib.Path(__file__).parent.absolute()
+    match_ids_path = here.parent / 'data/pro/pro_match_ids.txt'
 
-matches = filter(None, map(lambda x: re.match(r'.*(\d{9}).json$', x),
-                           os.listdir('../data/pro/')))
-matches = [x.groups()[0] for x in matches]
-new_matches = [x for x in match_ids if x not in matches]
-new_matches
+    with match_ids_path.open() as f:
+        old_ids = f.readlines()
 
-details = {mid: h.get_match_details(mid) for mid in new_matches}
-details = {}
-there = '../data/pro/'
+    ids = (x[2].split('?q=')[-1] + '\n' for x in links)
+    new_ids = [x for x in ids if x not in old_ids]
 
-try:
-    os.makedirs(there)
-except FileExistsError:
-    pass
+    with match_ids_path.open('a+') as f:
+        f.writelines(new_ids)
 
-for k in details:
-    with open(there + k + '.json', 'w') as f:
-        json.dump(details[k].resp, f)
+    #--------------------------------------------------------------------------
+    # Get Match Details for new matches
+    with open(os.path.expanduser('~/') + 'Dropbox/bin/api-keys.txt') as f:
+        key = json.load(f)['steam']
+
+    h = api.API(key=key)
+
+    with match_ids_path.open() as f:
+        match_ids = [x.strip() for x in f.readlines()]
+
+    f = pathlib.Path(__file__).absolute()
+    data_path = f.parent.joinpath(pathlib.Path('data/pro'))
+
+    matches = filter(None, map(lambda x: re.match(r'.*(\d{9}).json$', x),
+                               os.listdir(str(data_path))))
+    matches = [x.groups()[0] for x in matches]
+    new_matches = [x for x in match_ids if x not in matches]
+    details = {mid: h.get_match_details(mid) for mid in new_matches}
+
+    if not data_path.exists():
+        data_path.mkdir()
+
+    for k in details:
+        with (data_path / (str(k) + '.json')).open('w') as f:
+            json.dump(details[k].resp, f)
+
+    #--------------------------------------------------------------------------
+    # Insert into pro.db
+    from examples.sqlalchemyORM import update_db
+
+    update_db(data_path)
+
+    print("Added {}".format(new_ids))
+
+
+if __name__ == '__main__':
+    get_pro_matches()
