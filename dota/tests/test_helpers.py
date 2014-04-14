d1796edfc76b26b9f5faadbdb674548af999e1a7
TomAugspurger <thomas-augspurger@uiowa.edu>
2014-04-14 11:22:26 -0500

    update cached_games test to return Paths

--- dota/tests/test_helpers.py
+++ dota/tests/test_helpers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import pathlib
+from pathlib import Path
 import unittest
 import io
 
@@ -10,22 +10,22 @@ from dota.helpers import cached_games, open_or_stringIO
 class TestHelpers(unittest.TestCase):
 
     def setUp(self):
-        pathlib.Path('details12345678.json').touch()
-        pathlib.Path('1234.json').touch()
+        Path('details12345678.json').touch()
+        Path('1234.json').touch()
 
     def test_cached_games(self):
 
-        p = pathlib.Path('.')
+        p = Path('.')
         result = sorted(cached_games(p))
-        expected = [1234, 12345678]
+        expected = [Path('1234.json'), Path('details12345678.json')]
         self.assertEqual(result, expected)
 
         result = sorted(cached_games(p))
         self.assertEqual(result, expected)
 
     def tearDown(self):
-        pathlib.Path('details12345678.json').unlink()
-        pathlib.Path('1234.json').unlink()
+        Path('details12345678.json').unlink()
+        Path('1234.json').unlink()
 
     def test_open_or_stringIO(self):
         fileobj = open_or_stringIO('1234.json')
